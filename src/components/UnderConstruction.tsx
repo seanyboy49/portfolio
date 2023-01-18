50bf2137f31967577ea43a7d12931830ec9f5d12
Sean Lee <sean@Seans-MacBook-Pro.local>
2023-01-18 15:33:37 -0800

    add under construction

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -1,13 +1,20 @@
 import styled from "styled-components";
+import githubSrc from "../images/social/github.png";
+import linkedInSrc from "../images/social/linkedIn.png";
+import Icon from "./Icon";
 
-const Container = styled.div`
+const FlexContainer = styled.div`
   display: flex;
-  background: lightblue;
-  height: 100vh;
 `;
 
 const HeroText = styled.h1`
   font-size: 6rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const Text = styled.p`
+  font-size: 1.5rem;
 `;
 
 const Hero = styled.div`
@@ -17,15 +24,25 @@ const Hero = styled.div`
   text-align: center;
 `;
 
+const socialLinks = [
+  { iconSrc: githubSrc, href: "https://github.com/seanyboy49" },
+  { iconSrc: linkedInSrc, href: "https://www.linkedin.com/in/seanboramlee/" },
+];
+
 const UnderConstruction = () => {
   return (
-    <Container>
+    <FlexContainer style={{ height: "100vh" }}>
       <Hero>
         <HeroText>COMING SOON</HeroText>
-        <p>My portfolio website is still under construction.</p>
-        <p>In the mean time, here are some fun links to click</p>
+        <Text>My portfolio website is still under construction.</Text>
+        <Text>In the mean time, here are some fun links to click.</Text>
+        <FlexContainer style={{ justifyContent: "center", marginTop: "1rem" }}>
+          {socialLinks.map((link) => {
+            return <Icon {...link} />;
+          })}
+        </FlexContainer>
       </Hero>
-    </Container>
+    </FlexContainer>
   );
 };
 
