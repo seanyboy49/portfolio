727be8652f16156947529599d87edf8c6f8b3eb7
Sean Lee <sean@Seans-MacBook-Pro.local>
2023-01-18 22:45:43 -0800

    set up basic SpaceShooterGame

--- src/components/SpaceShooterGame.tsx
+++ src/components/SpaceShooterGame.tsx
@@ -1,8 +1,22 @@
 import { useCallback } from "react";
+import Player from "../games/SpaceShooter/Player";
+import SpaceShooterGame from "../games/SpaceShooter/SpaceShooterGame";
+import useCanvas from "../hooks/useCanvas";
 
 const SpaceShooterGameBoard = () => {
-  const setUpGame = useCallback((ctx: CanvasRenderingContext2D) => {}, []);
-  return <canvas />;
+  const setUpGame = useCallback((ctx: CanvasRenderingContext2D) => {
+    const x = ctx.canvas.width / 2;
+    const y = ctx.canvas.height / 2;
+
+    const player = new Player({ ctx, x, y, radius: 10, color: "white" });
+    const game = new SpaceShooterGame({ ctx, player });
+
+    return game;
+  }, []);
+
+  const canvas = useCanvas(setUpGame);
+
+  return <canvas ref={canvas} />;
 };
 
 export default SpaceShooterGameBoard;
