a7e421da51f9b094886824bb64a0fb0a4e475d65
Sean Lee <sean@Seans-MacBook-Pro.local>
2023-01-19 10:39:17 -0800

    spawn enemies function

---
+++ src/games/SpaceShooter/Enemy.tsx
@@ -0,0 +1,41 @@
+interface IEnemy {
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  velocity: { x: number; y: number };
+}
+
+class Enemy {
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  velocity: { x: number; y: number };
+
+  constructor({ ctx, x, y, radius, color, velocity }: IEnemy) {
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+export default Enemy;
