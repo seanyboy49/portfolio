bf989d5c28b3e1904ffae177d4a75aab5afb57d8
Sean Lee <sean@Seans-MacBook-Pro.local>
2023-02-15 21:50:16 -0800

    clean up

--- src/components/PaginationArrows.tsx
+++ src/components/PaginationArrows.tsx
@@ -4,25 +4,12 @@ import arrowRightSrc from "../images/RPG/arrow-right.png";
 import arrowLeftSrc from "../images/RPG/arrow-left.png";
 import IconButton from "./IconButton";
 
-// const OverflowContainer = styled.div`
-//   overflow-y: scroll;
-//   height: 100%;
-//   width: 100%;
-//   margin-top: 2rem;
-// `;
-
 const FlexContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
 `;
-
-// const TextContainer = styled.div`
-//   display: flex;
-//   padding-left: 1rem;
-//   padding-right: 1rem;
-// `;
 
 const Spacer = styled.div`
   width: 50px;
@@ -38,38 +25,6 @@ interface IPagination {
   handleClick: (direction: PaginationDirection) => void;
   children: React.ReactNode;
 }
-// const PaginationArrows = ({
-//   page,
-//   pagesLength,
-//   handleClick,
-//   children,
-// }: IPagination) => {
-//   return (
-//     <FlexContainer>
-//       {page !== 0 ? (
-//         <IconButton
-//           handleClick={() => handleClick(PaginationDirection.backward)}
-//           iconSrc={arrowLeftSrc}
-//           alt="Arrow pointed left"
-//         />
-//       ) : (
-//         <Spacer />
-//       )}
-//       <OverflowContainer>
-//         <TextContainer>{children}</TextContainer>
-//       </OverflowContainer>
-//       {page !== pagesLength ? (
-//         <IconButton
-//           handleClick={() => handleClick(PaginationDirection.forward)}
-//           iconSrc={arrowRightSrc}
-//           alt="Arrow pointed right"
-//         />
-//       ) : (
-//         <Spacer />
-//       )}
-//     </FlexContainer>
-//   );
-// };
 
 const PaginationArrows = ({
   page,
